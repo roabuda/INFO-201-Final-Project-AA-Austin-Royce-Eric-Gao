00aa13af0090eb0cdf6d337f96dd67d5adc262a9
Royce Abuda <roabuda@uw.edu>
2017-11-28 19:22:12 -0800

    join part of csv

---
+++ scripts/misc.R
@@ -0,0 +1,19 @@
+#load libraries
+library(rio)
+library(dplyr)
+
+#format population csv to only include state and population
+population <- read.csv("./data/Sheet1-Table 1.csv")
+population <- population[,c(1:3)]
+colnames(population) <- c("State","Votes","Population")
+population <- population %>% select(State,Population)
+
+#get party info
+party <- read.csv("./data/2016 Electoral Votes.csv")
+
+#change energy info to have states as rows and types of energy as columns
+energy <- import("./data/Net_generation_for_all_sectors.csv") %>% select(description, 'source key', as.character(2016)) %>% 
+  mutate()
+
+#join data
+joined <- party %>% left_join(population)
