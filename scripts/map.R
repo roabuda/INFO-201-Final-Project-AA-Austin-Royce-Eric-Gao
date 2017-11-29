078dd5edd6e23aaf965cbcd4346c204b53c2ee26
Royce Abuda <roabuda@uw.edu>
2017-11-29 14:52:49 -0800

    put in joined.csv

---
+++ scripts/map.R
@@ -0,0 +1,15 @@
+#get joined data
+library(dplyr)
+library(plotly)
+library(ggplot2)
+joined <- read.csv("./data/joined.csv")
+
+#create a function that takes in numbers for min and max energy, 
+#a vector of what types of energy to compare, and shows each
+#state's population and which party won in 2016
+
+create.map <- function(min,max,energy)
+{
+  
+}
+
