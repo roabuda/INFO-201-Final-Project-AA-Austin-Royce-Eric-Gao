3af3edeb720d0b33dc56f2e307e061d1107b3240
Royce Abuda <roabuda@uw.edu>
2017-11-29 16:01:39 -0800

    do part of map

--- scripts/map.R
+++ scripts/map.R
@@ -1,15 +1,41 @@
-#get joined data
+#load libraries
 library(dplyr)
 library(plotly)
 library(ggplot2)
-joined <- read.csv("./data/joined.csv")
 
-#create a function that takes in numbers for min and max energy, 
+#get joined data
+joined <- read.csv("./data/joined.csv", stringsAsFactors = FALSE)
+joined$State <- toupper(joined$State)
+
+#create min,max, and energy values for testing
+min <- 0
+max <- 1000
+energy <- c("PC.","GEO")
+
+#create a function that uses the joined data and takes in numbers for min and max energy, 
 #a vector of what types of energy to compare, and shows each
 #state's population and which party won in 2016
 
 create.map <- function(min,max,energy)
 {
+  #load US map data
+  states <- map_data("state") %>% select(-subregion, -order)
+  colnames(states) <- c("long","lat","group","State")
+  states$State <- toupper(states$State)
   
+  #plot states with ggplot
+  state.base <- ggplot()
+  
+  #create map of the US with state boundaries
+  state.base <- state.base + geom_polygon( data=states, aes(x=long, y=lat, group = group),colour="white") + 
+    coord_fixed(1.3)
+  
+  #join the states data and the joined data to create the map data
+  map.data <- inner_join(states, joined, by = "State") 
+  
+  
+   state.map <- state.base + 
+    geom_polygon(data = map.data, aes(fill = Population), color = "white") +
+    geom_polygon(color = "black", fill = NA) + theme_void()
 }
 
