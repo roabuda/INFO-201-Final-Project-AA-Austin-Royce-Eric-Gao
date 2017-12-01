0a896bcf759b8b61ced0819c2e5788c64804ad68
Xiaxuan  Gao <xiaxuangao@D-10-157-52-112.dhcp4.washington.edu>
2017-11-30 21:19:47 -0800

    fix the problem in the histogram function

--- scripts/histogram.R
+++ scripts/histogram.R
@@ -1,30 +1,43 @@
 library(ggplot2)
 library(dplyr)
-joined.data <- read.csv("./data/joined.csv")
+library(MASS)
+
 HistogramGraph <- function(data.frame, 
-                           xvar, 
-                           yvar, 
-                           my.title,
-                           x.lab,
-                           y.lab,
-                           colorVar,
-                           x.max = min(x.var),
-                           y.max = max(y.var),
-                           x.min = 0,
-                           y.min = 0) {
-  #change -- as 0
-  joined[joined == "--" | joined == "NM"] <- 0
-  
- p <- ggplot(data = data.frame, aes(x=xvar, y=yvar))+
-    geom_bar(stat = "identity")+
-    geom_text(aes(y = yvar, label=yvar), vjust=1.6, 
-              color="Black", size=3.5)+
-    # scale_fill_brewer(palette = energy.type)+
-    ggtitle(my.title)+
-    labs(x=x.lab, y=y.lab)+
-    xlim(x.min, x.max)+
-    ylim(y.min, y.max)+
-    theme_minimal()
+                           y.var, 
+                           my.title = "Tittle",
+                           y.lab = "Y Lab",
+                           legend.title = "Legend Lab") {
+p <- ggplot(data = data.frame,
+            aes(x = factor(State, levels = State[order(Population)]), data.frame[,y.var], fill = data.frame[,y.var])) +
+     geom_histogram(stat = "identity")+
+     # geom_text(aes(label=joined$COW), vjust=-0.3, size=2.5)+
+     labs(x = "State", y = y.lab, title = my.title, fill = legend.title)+
+     # ylim(y.min, y.max)+
+     coord_flip()+
+     theme(axis.text.y.left = element_text(face="bold", color = "black", 
+                                        size=8))
+ 
   return(p)
+
 }
-HistogramGraph(joined, xvar = joined$Population, yvar = joined$BIO, my.title = "test", x.lab = "Population", y.lab = "BIO",colorVar = joined$BIO, x.max = max(joined$Population), y.max = max(joined$BIO), x.min = 0, y.min = 0)
+
+#TEST
+# ptest2 <- ggplot(data = joined,
+#             aes(x = factor(State, levels = State[order(Population)]),as.numeric(BIO), fill = as.numeric(BIO))) +
+#   geom_histogram(stat = "identity")+
+#   # geom_text(aes(label=joined$COW), vjust=-0.3, size=2.5)+
+#   labs(aes(x = "State", y = y.lab, title = my.title, fill = legend.title))+
+#   # ylim(y.min, y.max)+
+#   coord_flip()+
+#   theme(axis.text.y.left = element_text(face="bold", color = "black", 
+#                                         size=8))
+# 
+# ptest2
+
+#Sample Code
+HistogramGraph(data.frame = joined, 
+               y.var = "COW", 
+               my.title = "TEST",
+               y.lab = "COW",
+               legend.title = "COW")
+
