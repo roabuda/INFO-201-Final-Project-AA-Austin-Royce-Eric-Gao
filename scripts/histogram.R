a528c3e10160c61267625e33b8cf1a68338fccba
Xiaxuan  Gao <xiaxuangao@D-10-157-52-112.dhcp4.washington.edu>
2017-12-03 15:46:55 -0800

    set the y value in a decreasing order

--- scripts/histogram.R
+++ scripts/histogram.R
@@ -16,9 +16,9 @@ HistogramGraph <- function(data.frame,
                            y.lab = "Y Lab",
                            legend.title = "Legend Lab") {
 p <- ggplot(data = data.frame,
-            aes(x = factor(State, levels = State[order(Population)]), data.frame[,y.var], fill = data.frame[,y.var])) +
+            aes(x = factor(State, levels = State[order(data[,y.var], decreasing = TRUE)]), data.frame[,y.var], fill = data.frame[,y.var])) +
      geom_histogram(stat = "identity")+
-     geom_text(aes(label=data.frame[,y.var]), vjust=-0.3, size=2.5)+
+     # geom_text(aes(label=data.frame[,y.var]), vjust=-0.3, size=2)+
      labs(x = "State", y = y.lab, title = my.title, fill = legend.title)+
      # coord_cartesian(ylim = c(0, max(data.frame$y.var)))+
      # coord_flip()+
@@ -30,24 +30,26 @@ p <- ggplot(data = data.frame,
 }
 
 #### TEST ####
-# ptest2 <- ggplot(data = joined,
-#             aes(x = factor(State, levels = State[order(Population)]),as.numeric(BIO), fill = as.numeric(BIO))) +
-#   geom_histogram(stat = "identity")+
-#   # geom_text(aes(label=joined$COW), vjust=-0.3, size=2.5)+
-#   labs(aes(x = "State", y = y.lab, title = my.title, fill = legend.title))+
-#   # ylim(y.min, y.max)+
-#   coord_flip()+
-#   theme(axis.text.y.left = element_text(face="bold", color = "black", 
-#                                         size=8))
-# 
-# ptest2
+ptest1 <- ggplot(data = joined,
+            aes(x = factor(State, levels = State[order(joined[,"COW"], decreasing = TRUE)]), joined[,"COW"], fill = joined[,"COW"])) +
+  geom_histogram(stat = "identity")+
+  geom_text(aes(label=joined[,"COW"]), vjust=-0.3, size=2)+
+  labs(x = "State", y = "COW", title = "my.title", fill = "legend.title")+
+  # coord_cartesian(ylim = c(0, max(data.frame$y.var)))+
+  # coord_flip()+
+  theme(axis.text.x = element_text(face="bold", color="#993333", 
+                                   size=10, angle=90))
+ptest1
+p <- ptest1+ geom_line(data=joined, aes(x=as.numeric(State), y=joined[,"COW"]), colour = "red")
+p
+
 
 #Sample Code
 HistogramGraph(data.frame = joined, 
-               y.var = "COW", 
+               y.var = "BIO", 
                my.title = "TEST",
-               y.lab = "COW",
-               legend.title = "COW")
+               y.lab = "BIO",
+               legend.title = "BIO")
 
 
 
