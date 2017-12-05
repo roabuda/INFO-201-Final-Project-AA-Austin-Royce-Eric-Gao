f317a41ee3ea2a0fa4dfb7aa8ba7eb010909d3e3
Xiaxuan  Gao <xiaxuangao@D-10-157-52-112.dhcp4.washington.edu>
2017-12-05 11:15:42 -0800

    filter our 0 number count in pie chart

--- scripts/histogram.R
+++ scripts/histogram.R
@@ -31,7 +31,7 @@ HistogramLineGraph <- function(data.frame,
   
 #make the histogram  
   p1 <- plot_ly(data = data.frame, x = ~data.frame[,y.var], y = ~reorder(State, data.frame[,y.var]), name = bar.title,
-                type = 'bar', orientation = 'h',text = ~y.var, textposition = 'auto',
+                type = 'bar', orientation = 'h',text = ~data.frame[,y.var], textposition = 'auto',
                 marker = list(color = 'rgba(50, 171, 96, 0.6)',
                               line = list(color = 'rgba(50, 171, 96, 1.0)', width = 1))) %>%
     layout(yaxis = list(showgrid = FALSE, showline = FALSE, showticklabels = TRUE, domain= c(0, 0.85),title = "STATE",
@@ -42,7 +42,7 @@ HistogramLineGraph <- function(data.frame,
            xaxis = list(zeroline = FALSE, showline = FALSE, showticklabels = TRUE, showgrid = TRUE)) 
  
 #make the line graph
-  p2 <- plot_ly(data = data.frame, x = ~data.frame[,y.var], y = ~State, name = line.title,
+  p2 <- plot_ly(data = data.frame, x = ~data.frame[,y.var], y = ~reorder(State, data.frame[,y.var]), name = line.title,
                 type = 'scatter', mode = 'lines+markers',
                 line = list(color = 'rgb(128, 0, 128)')) %>%
     layout(yaxis = list(showgrid = FALSE, showline = TRUE, showticklabels = FALSE,
@@ -73,27 +73,27 @@ HistogramLineGraph(data.frame = joined,
                    line.title = "line_title")
 
 # #### TEST ####
-# # ptest1 <- ggplot(data = joined,
-# #             aes(x = factor(State, levels = State[order(joined[,"COW"], decreasing = TRUE)]), joined[,"COW"], fill = joined[,"COW"])) +
-# #   geom_histogram(stat = "identity")+
-# #   geom_text(aes(label=joined[,"COW"]), vjust=-0.3, size=2)+
-# #   labs(x = "State", y = "COW", title = "my.title", fill = "legend.title")+
-# #   # coord_cartesian(ylim = c(0, max(data.frame$y.var)))+
-# #   # coord_flip()+
-# #   theme(axis.text.x = element_text(face="bold", color="#993333", 
-# #                                    size=10, angle=90))
-# # ptest1
-# # p <- ptest1+ geom_line(data=joined, aes(x=as.numeric(State), y=joined[,"COW"]), colour = "red")
-# # p
-# m <- list(
-#   l = 30,
-#   r = 30,
-#   b = 80,
-#   t = 80,
-#   pad = 4
-# )
-# 
-# p <- plot_ly(joined, y = ~factor(State, levels = State[order(joined[,'COW'], decreasing = F)]), x = ~COW, type = 'bar', 
+# ptest1 <- ggplot(data = joined,
+#             aes(x = factor(State, levels = State[order(joined[,"COW"], decreasing = TRUE)]), joined[,"COW"], fill = joined[,"COW"])) +
+#   geom_histogram(stat = "identity")+
+#   geom_text(aes(label=joined[,"COW"]), vjust=-0.3, size=2)+
+#   labs(x = "State", y = "COW", title = "my.title", fill = "legend.title")+
+#   # coord_cartesian(ylim = c(0, max(data.frame$y.var)))+
+#   # coord_flip()+
+#   theme(axis.text.x = element_text(face="bold", color="#993333",
+#                                    size=10, angle=90))
+# ptest1
+# p <- ptest1+ geom_line(data=joined, aes(x=as.numeric(State), y=joined[,"COW"]), colour = "red")
+# p
+m <- list(
+  l = 30,
+  r = 30,
+  b = 80,
+  t = 80,
+  pad = 4
+)
+
+# p <- plot_ly(joined, y = ~factor(State, levels = State[order(joined[,'COW'], decreasing = F)]), x = ~COW, type = 'bar',
 #              text = ~BIO, textposition = 'auto',orientation = 'h',
 #              marker = list(color = 'rgb(158,202,225)',
 #                            line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
@@ -103,8 +103,8 @@ HistogramLineGraph(data.frame = joined,
 # 
 # p
 # #Sample Code
-# HistogramGraph(data.frame = joined, 
-#                y.var = "BIO", 
+# HistogramGraph(data.frame = joined,
+#                y.var = "BIO",
 #                my.title = "TEST",
 #                y.lab = "BIO",
 #                legend.title = "BIO")
@@ -139,7 +139,7 @@ HistogramLineGraph(data.frame = joined,
 #                       showticklabels = TRUE,
 #                       tickfont = f2,
 #                       exponentformat = "E"),
-#          xaxis = list(zeroline = FALSE, showline = FALSE, showticklabels = TRUE, showgrid = TRUE)) 
+#          xaxis = list(zeroline = FALSE, showline = FALSE, showticklabels = TRUE, showgrid = TRUE))
 #   # add_annotations(xref = 'x1', yref = 'y',
 #   #                 # x = COW * 2.1 + 3,  y = y,
 #   #                 text = ~COW,
@@ -153,7 +153,7 @@ HistogramLineGraph(data.frame = joined,
 #                       linecolor = 'rgba(102, 102, 102, 0.8)', linewidth = 2,
 #                       domain = c(0, 0.85)),
 #          xaxis = list(zeroline = FALSE, showline = FALSE, showticklabels = TRUE, showgrid = TRUE,
-#                       side = 'top', dtick = 25000)) 
+#                       side = 'top', dtick = 25000))
 #   # add_annotations(xref = 'x2', yref = 'y',
 #   #                 x = x_net_worth, y = y,
 #   #                 text = paste(x_net_worth, 'M'),
@@ -166,7 +166,7 @@ HistogramLineGraph(data.frame = joined,
 #                        font = list(size = 10)),
 #          margin = list(l = 100, r = 20, t = 70, b = 70),
 #          paper_bgcolor = 'rgb(248, 248, 255)',
-#          plot_bgcolor = 'rgb(248, 248, 255)') 
+#          plot_bgcolor = 'rgb(248, 248, 255)')
 #   # add_annotations(xref = 'paper', yref = 'paper',
 #   #                 x = -0.14, y = -0.15,
 #   #                 text = paste('OECD (2015), Household savings (indicator), Household net worth (indicator). doi: 10.1787/cfc6f499-en (Accessed on 05 June 2015)'),
