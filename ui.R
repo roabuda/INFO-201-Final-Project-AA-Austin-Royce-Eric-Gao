6850a47ee26ed87f9e96d2c18367ba51901300a1
Eric Acero <ebar23@uw.edu>
2017-12-05 17:16:40 -0800

    edited the ui

--- ui.R
+++ ui.R
@@ -24,33 +24,48 @@ navbarPage(
   tabPanel("Introduction",
  
     mainPanel(
-      h2("Energy Consumption in the United States"),
-        tags$hr(),
-          tags$img(src = "https://www.carbonbrief.org/wp-content/uploads/2016/05/Stock-Fort-McMurray.jpg", width = "800px", height = "300px"),
-      h3("Project Overview"),
-      h5("For our project, we wanted to observe the use of energy throughout the United States and to see how it correlated to the political inclination and population of each state. We were able to observe this connection with the use of two data sets that we later combined into one."),
-      h3("Audience"),
-      h5("Our target audience for our Final Project are people interested in energy usage and its correlation to seemingly unrelated things (political inclination and population), such as environmentalists. Environmentalists would be interested in this data because it could help them understand why possibly harmful energy (fossil fuels) are used more in certain states versus clean energy usage. This could help think of better ways to address energy usage depending on which state has a bigger problem with the kind of energy they’re using."),
-      h3("Data"),
-      h5("For our data, we used a data list from the US Energy Information Administration that included all the energy usage data throughout the states from 2016. We also got the electoral college results from the 2016 election from data.world."),
-      h3("Questions"),
-      h5("-Do more populated states use more clean energy than those that aren’t as populated?",
-         tags$br(),
-          "-Does the energy consumption by each state correlate to its political inclination?",
-         tags$br(),
-          "-What energy is used the most in each state?"),
-      h3("Structure"),
-      h5("Our tabs include: an interactive map that shows the political inclination of each state and will also display energy information in the form of a pie chart when you hover over each state, a histogram that displays the amount of energy use per state, and a scatter plot that shows the wattage for a population."),
-      h3("Creators"),
-      h5("Austin Chan",
-          tags$br(),
-           "Xiaxuan Gao", 
-          tags$br(),
-           "Royce Abuda", 
-          tags$br(),
-           "Eric Acero")
-      )
+      tags$blockquote(
+        h2("Energy Consumption in the United States"),
+          tags$hr(),
+            tags$img(src = "https://www.carbonbrief.org/wp-content/uploads/2016/05/Stock-Fort-McMurray.jpg", width = "800px", height = "300px"),
+        h3("Project Overview"),
+        h5("For our project, we wanted to observe the use of energy throughout the United States and to see how it correlated to the political inclination and population of each state. We were able to observe this connection with the use of two data sets that we later combined into one."),
+        h3("Audience"),
+        h5("Our target audience for our Final Project are people interested in energy usage and its correlation to seemingly unrelated things (political inclination and population), such as environmentalists. Environmentalists would be interested in this data because it could help them understand why possibly harmful energy (fossil fuels) are used more in certain states versus clean energy usage. This could help think of better ways to address energy usage depending on which state has a bigger problem with the kind of energy they’re using."),
+        h3("Data"),
+        h5("For our data, we used a data list from the US Energy Information Administration that included all the energy usage data throughout the states from 2016. We also got the electoral college results from the 2016 election from data.world."),
+        h3("Questions"),
+        h5("-Do more populated states use more clean energy than those that aren’t as populated?",
+           tags$br(),
+            "-Does the energy consumption by each state correlate to its political inclination?",
+           tags$br(),
+            "-What energy is used the most in each state?"),
+        h3("Structure"),
+        h5("Our tabs include: an interactive map that shows the political inclination of each state and will also display energy information in the form of a pie chart when you hover over each state, a histogram that displays the amount of energy use per state, and a scatter plot that shows the wattage for a population."),
+        h3("Creators"),
+        h5("Austin Chan",
+            tags$br(),
+             "Xiaxuan Gao", 
+            tags$br(),
+             "Royce Abuda", 
+            tags$br(),
+             "Eric Acero")
+          )
+        )
       ),
+  tabPanel("Why This Matters?",
+           tags$blockquote(
+             h3("Global Warming...Climate Change...it's real"),
+             tags$hr(),
+             tags$img(src = "https://www.popsci.com/sites/popsci.com/files/earth_from_the_iss.jpg", width = "420px", height = "300px"),
+             tags$img(src = "https://ecochiccayman.files.wordpress.com/2015/02/melting-ice-polar-bear-on-206311.jpg", width = "420px", height = "300px"),
+             h5("In our world today, we are faced with an issue. An issue that will define the course of history in the years to come. An issue that will affect not just us humans but also every being on this Earth. This issue is commonly known as Global Warming."),
+             h5("It is as it sounds-- the warming of our world. This is detrimental to us in the sense that with our climate rising in temperature, our polar ice caps are melting and therefore, the ocean levels are rising as well. This affects the coasts of all the continents and could leave millions, if not billions homeless if the ocean levels flooded major cities."),
+             h5("The reason the Earth is heating up is because of carbon emissions and other natural gases that get released into our atmosphere every single day. Since the Industrial Revolution, factories, power plants and later cars have released these gases that trap the Sun's energy in our atmosphere and prevent it from escaping into space as it should. This in return heats up the Earth. This is known as the Green House Effect."),
+             h5("And it's not just us humans. Animals could go extinct if their habitats are affected by the global climate change. Polar Bears are Endangered Species to this date with the melting of the Northern ice cap. Animals are having to migrate to different areas of the world to try and save their existence. But sometimes the transition to a new territory isn't so smooth. They might not get used to the new environment or they might act as an invasive species that could dismantal the food chain in that territory. In the end, all of this are the reprecussions of global warming. We are altering the balance of nature and in return we will end up not only causing the extinction of animals, but also our own."),
+             h5("That is why we also created this report, to educate the common folk of the occurances of what is going on in our world today. We are unfortunately lead by an authoritative figure (*cough *cough Donald Trump) that doesn't believe that the issue of Global Warming is worth our time. But you know what, we don't need him to believe in it so long as the greater population does and acts upon it to make a difference in saving this great planet we call home. ")
+            )
+          ),
 
   tabPanel("Map/Pie Chart",
            sidebarLayout(
@@ -137,7 +152,7 @@ navbarPage(
            )
   ),
   
-  tabPanel("Change in energy usage",
+  tabPanel("Change in Energy Usage",
            sidebarLayout(
              sidebarPanel(
                selectInput("hist.var.c", "What do you want to histogram?", 
@@ -231,29 +246,21 @@ navbarPage(
              )
            )
   ),
-  tabPanel("Why This Matters?",
-    h3("Global Warming...Climate Change...it's real"),
-      tags$hr(),
-        tags$img(src = "https://www.popsci.com/sites/popsci.com/files/earth_from_the_iss.jpg", width = "437px", height = "300px"),
-            tags$img(src = "https://ecochiccayman.files.wordpress.com/2015/02/melting-ice-polar-bear-on-206311.jpg", width = "437px", height = "300px"),
-    h5("In our world today, we are faced with an issue. An issue that will define the course of history in the years to come. An issue that will affect not just us humans but also every being on this Earth. This issue is commonly known as Global Warming."),
-    h5("It is as it sounds-- the warming of our world. This is detrimental to us in the sense that with our climate rising in temperature, our polar ice caps are melting and therefore, the ocean levels are rising as well. This affects the coasts of all the continents and could leave millions, if not billions homeless if the ocean levels flooded major cities."),
-    h5("The reason the Earth is heating up is because of carbon emissions and other natural gases that get released into our atmosphere every single day. Since the Industrial Revolution, factories, power plants and later cars have released these gases that trap the Sun's energy in our atmosphere and prevent it from escaping into space as it should. This in return heats up the Earth. This is known as the Green House Effect."),
-    h5("And it's not just us humans. Animals could go extinct if their habitats are affected by the global climate change. Polar Bears are Endangered Species to this date with the melting of the Northern ice cap. Animals are having to migrate to different areas of the world to try and save their existence. But sometimes the transition to a new territory isn't so smooth. They might not get used to the new environment or they might act as an invasive species that could dismantal the food chain in that territory. In the end, all of this are the reprecussions of global warming. We are altering the balance of nature and in return we will end up not only causing the extinction of animals, but also our own."),
-    h5("That is why we also created this report, to educate the common folk of the occurances of what is going on in our world today. We are unfortunately lead by an authoritative figure (*cough *cough Donald Trump) that doesn't believe that the issue of Global Warming is worth our time. But you know what, we don't need him to believe in it so long as the greater population does and acts upon it to make a difference in saving this great planet we call home. ")
-  ),
+  
   tabPanel("Sources",
   
   mainPanel(
-    h3("These are the data resources that we used for the purpose of this report:"),
-      tags$hr(),
-    h1(" "),
-    h4("US Energy Information Administration"),
-      tags$a(href="https://www.eia.gov/", "Click here for the link"),
-    h4("data.world"),
-      tags$a(href="https://data.world/dash/2016-electoral-college-results", "Click here for the link"),
-    h4("United States Census Bureau"),
-      tags$a(href="https://www.census.gov/data/tables/2016/demo/popest/state-total.html", "Click here for the link")
+    tags$blockquote(
+      h3("These are the data resources that we used for the purpose of this report:"),
+        tags$hr(),
+      h1(" "),
+      h4("US Energy Information Administration"),
+        tags$a(href="https://www.eia.gov/", "Click here for the link"),
+      h4("data.world"),
+        tags$a(href="https://data.world/dash/2016-electoral-college-results", "Click here for the link"),
+      h4("United States Census Bureau"),
+        tags$a(href="https://www.census.gov/data/tables/2016/demo/popest/state-total.html", "Click here for the link")
+        )
     )
   )
 )
